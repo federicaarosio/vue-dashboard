092086aa0382b7c941e37651d5e247134d47df89
federicaarosio <arosio.fede@gmail.com>
2024-03-22 18:19:48 +0100

    general scaffolding, added scss and bootstrap

---
+++ src/components/AppAside.vue
@@ -0,0 +1,14 @@
+<template>
+    <aside>
+        Aside
+    </aside>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="">
+    
+</style>
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from '../App.vue'
+import '../styles/general.scss'
+import 'bootstrap/scss/bootstrap.scss'
+import 'bootstrap/js/index.umd'
+
+createApp(App).mount('#app')
